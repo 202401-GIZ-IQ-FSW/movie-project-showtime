5dd5c37db272bff297a5e7cbadd7578ce1803c66
elaf <elafghassangardi@gmail.com>
2024-03-20 00:04:38 +0300

    fetched api using url and pass it down to Home component

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,28 @@
-export default function Home() {
+"use client"
+import React, {useState, useEffect} from "react";
+import Home from './components/Home'
+
+export default function Page() {
+  const [latestMovies, setLatestMovies] = useState([]);
+
+    const fetchData = async () =>{
+        try {
+            const response = await fetch("https://api.themoviedb.org/3/discover/movie?api_key=17ffe7213d2cc56b18d9382aff562804");
+            const data = await response.json();
+            setLatestMovies(data);
+        }
+        catch(error){
+            console.error('Error fetching posts:', error);
+        }
+    }
+
+    useEffect(()=>{
+        fetchData();
+    }, [] )
+
   return (
-    <div className="flex justify-center items-center py-36">
-      <h1 className="text-blue-500 font-bold">Hello World</h1>
-    </div>
+    <>
+      <Home data={latestMovies}/>
+    </>
   )
 }
