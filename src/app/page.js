a9f39d467ce654c3ee81e00751fddd344a837422
elaf <elafghassangardi@gmail.com>
2024-03-20 16:56:49 +0300

    fixed merge issues

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,10 @@
-export default function Home() {
+import React from "react"
+function Page() {
   return (
-    <div className="flex justify-center items-center py-36">
-      <h1 className="text-blue-500 font-bold">Hello World</h1>
+    <div>
+      <h1>...</h1>
     </div>
   )
 }
+
+export default Page;
